fafcde776f1600eaa0ff464625d89a49fc08ce11
sxin <boss@mrsong.xin>
2016-05-08 19:21:11 +0800

    TestServlet

--- src/javascr/me/test/TestServlet.java
+++ src/javascr/me/test/TestServlet.java
@@ -1,12 +1,20 @@
 package me.test;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by mereyu on 2016-05-07.
  */
 public class TestServlet extends HttpServlet{
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("我也要拦截！");
+    }
 }
